db8ac76de717f8232ccdb12aac6093131e22b84e
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2022-04-05 19:12:53 -0400

    feat(contract): initialize count

--- myepicproject/programs/myepicproject/src/lib.rs
+++ myepicproject/programs/myepicproject/src/lib.rs
@@ -9,9 +9,28 @@ pub mod myepicproject {
     use super::*;
 
     pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result<()> {
+        // Get a reference to the account.
+        let base_account = &mut ctx.accounts.base_account;
+        
+        // Initialize total_gifs.
+        base_account.total_gifts = 0;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct StartStuffOff {}
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program <'info, System>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifts: u64,
+}
