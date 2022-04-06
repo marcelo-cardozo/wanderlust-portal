b145a56d035184289305a705959189e27c9fbc97
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2022-04-06 19:08:22 -0400

    feat(contract): setup devnet

--- myepicproject/programs/myepicproject/src/lib.rs
+++ myepicproject/programs/myepicproject/src/lib.rs
@@ -1,7 +1,7 @@
 // import anchor tools
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("5NXdBN5s3pj6vVi4x6DcyEGPGtuFp4vhMHn9qSXUSppp");
 
 // extend program and include myepicproject module
 #[program]
